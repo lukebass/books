2ddb314bf2562360db5215139c26268efeec284f
Luke J. Basso <lukejbass@gmail.com>
2024-01-15 23:29:48 -0800

    Part Uno

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+
 function App() {
-    return <div>App</div>;
+    const [books, setBooks] = useState([]);
+
+    const deleteBookById = (id) => {
+        setBooks(books.filter((book) => book.id !== id))
+    };
+
+    const editBookById = (id, title) => {
+        setBooks(books.map((book) => {
+            if (book.id === id) return { ...book, title }            
+            return book;
+        }))
+    };
+
+    const createBook = (title) => {
+        setBooks([
+            ...books,
+            { 
+                id: Math.round(Math.random() * 1000), 
+                title 
+            }
+        ])
+    };
+
+    return (
+        <div className='app'>
+            <h1>Reading List</h1>
+            <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+            <BookCreate onCreate={createBook} />
+        </div>
+        
+    )
 }
 
 export default App;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,11 @@
-function BookList() {
-    return <div>BookList</div>;
+import BookShow from './BookShow';
+
+function BookList({ books, onDelete, onEdit }) {
+    return (
+        <div className='book-list'>
+            {books.map((book) => <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />)}
+        </div>
+    );
 }
 
 export default BookList;
